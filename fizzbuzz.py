e3f6ed221455989213fddba88891bba5f2b739f2
Ben <benranson5@gmail.com>
2021-03-25 15:58:50 +0000

    added oop fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,32 @@
+
+
+class FizzBuzz:
+
+    def __init__(self):
+        self.fizz = 3
+        self.buzz = 5
+
+    def is_fizz(self, value):
+        return value % self.fizz == 0
+
+    def is_buzz(self, value):
+        return value % self.buzz == 0
+
+    def is_fizz_buzz(self, value):
+        return self.is_fizz(value) and self.is_buzz(value)
+
+    def gen_100(self):
+        for num in range(1, 101, 1):
+            if self.is_fizz(num):
+                print("fizz")
+            elif self.is_buzz(num):
+                print("buzz")
+            elif self.is_fizz_buzz(num):
+                print("fizzbuzz")
+            else:
+                print(num)
+
+
+if __name__ == "__main__":
+    fizz_buzz = FizzBuzz()
+    fizz_buzz.gen_100()
