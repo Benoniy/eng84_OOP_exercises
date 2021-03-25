d41bb918d86c0e55a364acb5193de322dc1d03d7
Ben <benranson5@gmail.com>
2021-03-25 16:26:13 +0000

    added commenting

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,28 +3,31 @@
 class FizzBuzz:
 
     def __init__(self):
-        self.fizz = 3
-        self.buzz = 5
+        self.fizz = 3  # divisible by number for fizz
+        self.buzz = 5  # divisible by number for buzz
 
+    # Checks if a number is fizz
     def is_fizz(self, value):
         return value % self.fizz == 0
 
+    # Checks if a number is buzz
     def is_buzz(self, value):
         return value % self.buzz == 0
 
+    # Checks if a number is fizz and is buzz
     def is_fizz_buzz(self, value):
         return self.is_fizz(value) and self.is_buzz(value)
 
+    # Generates 1 to 100 in order and checks if they are fizz or buzz using the object methods
     def gen_100(self):
-        for num in range(1, 101, 1):
-            if self.is_fizz_buzz(num):
+        for num in range(1, 101, 1):  # loop from 1 to 100
+            if self.is_fizz_buzz(num):  # is it fizz and buzz?
                 print("fizzbuzz")
-            elif self.is_buzz(num):
+            elif self.is_buzz(num):  # is it buzz?
                 print("buzz")
-            elif self.is_fizz(num):
+            elif self.is_fizz(num):  # is it fizz?
                 print("fizz")
-
-            else:
+            else:  # is it none of the above?
                 print(num)
 
 
--- scrabble_word_calculator.py
+++ scrabble_word_calculator.py
@@ -3,25 +3,30 @@
 class ScrabbleCalc:
 
     def __init__(self):
+        # Dictionary that contains letter and their scores
         self.letter_vals = {"a": 1, "b": 3, "c": 3, "d": 2, "e": 1, "f": 4,
                             "g": 2, "h": 4, "i": 1, "j": 8, "k": 5, "l": 1,
                             "m": 3, "n": 1, "o": 1, "p": 3, "q": 10, "r": 1,
                             "s": 1, "t": 1, "u": 1, "v": 4, "w": 4, "x": 8,
                             "y": 4, "z": 10
                             }
-        self.word = ""
-        self.score = 0
+        self.word = ""  # For storing the original word
+        self.score = 0  # Tracks the score
 
+    # Used to calculate the score of a word
     def calculate_score(self, word):
-        self.word = word
-        lower = word.lower().strip()
+        self.word = word  # Store the original
+        lower = word.lower().strip()  # Format the word for processing
 
+        # Loop through the word and add the score for each char
         for char in lower:
             self.score += self.letter_vals[char]
 
+        # Print the value of the word
         print(f"{word} is worth {self.score} points!")
         self.reset_calc()
 
+    # Reset the score and word for next time
     def reset_calc(self):
         self.word = ""
         self.score = 0
